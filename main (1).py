a1f37d48819eb805d5197bb858b8ba1bb1a8b334
said tatou <saidtatou44@gamail.com>
2024-11-06 16:41:00 -0800

    delete function done ,code tested

--- main (1).py
+++ main (1).py
@@ -5,7 +5,7 @@ class Student:
 
     def __init__(self, id, name):    
         self.id = id
-        self.name = name    
+        self.name = name           
 
         Student.numofStudent = Student.numofStudent + 1 # increament 'numofStudent , when a new student is cerated.
 
@@ -35,12 +35,12 @@ class Student:
         else:
             self._name = newname
 
-
-    def __str__(self):     # string representation method.
+    # string representation method of the student object:
+    def __str__(self):     
         return (f'Student id: {self.id:>10} \t name: {self.name:>10}')
    
 
-# Function to create a list of students objects from a list of dictionaries: 
+# Function to create a list of student objects from the student list of dictionaries: 
 def makeStudent(student_dict_list):
 
     students_list = []   # to hold student objects   
@@ -58,11 +58,29 @@ def makeStudent(student_dict_list):
 
     return students_list
 
-
+# Function to delete a student object form with a given ID number:
 def deleteOneStudent(slist, did):
-    print()
+    
+    # making a new list of students excluding the student that has id = did:  
+    new_list = list(filter(lambda student: student.id != did, slist))
 
+    # checking if the original list 'slist' is changed:
+    if len(new_list) != len(slist):  # if True, means the selected student by id is deleted
 
+        # update the original list in place:
+        slist[:] = new_list
+
+        # since one student is deleted , decrease the the number of student objects here:
+        Student.numofStudent = Student.numofStudent - 1
+        
+        # printing the user feedback after the delete:
+        print('\nThe student with ID', did, 'was successfully deleted.')
+
+    else:   # if the id was not found( same length for both list )
+        print('\nstudent with ID equal to', did, 'was not found.')    
+
+  
+    
 def main():
 
     student_dict_list = [{'id': 1001, 'name': 'John'},
@@ -70,14 +88,19 @@ def main():
                          {'id': 1003, 'name': 'Mark'},
                          {'id': 1004, 'name': 'Matthew'},
                          {'id': 1005, 'name': 'Arnold'}]
+
     slist = makeStudent(student_dict_list)
+
     print(f'\n**** Total number of students: {Student.numofStudent}')
     for s in slist:
         print(s)
 
     # Delete one object
+
     did = 1003
+
     deleteOneStudent(slist, did)
+
     print(f'\n**** Total number of students: {Student.numofStudent}')
     for s in slist:
         print(s)
